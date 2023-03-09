bf1287f096006468465ddbb344858e696011c54d
Chuma Mukala <chumamukala@gmail.com>
2023-03-09 04:12:14 +0100

    new restaurant model

--- db/migrate/20230309031030_create_restaurants.rb
+++ db/migrate/20230309031030_create_restaurants.rb
@@ -4,7 +4,6 @@ class CreateRestaurants < ActiveRecord::Migration[7.0]
       t.string :name
       t.string :address
       t.string :phone_number
-      t.string :category
 
       t.timestamps
     end
